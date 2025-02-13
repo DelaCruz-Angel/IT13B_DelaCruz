283458fa76f7524d9b91075a99fe120eb39f1652
DelaCruz-Angel <mobilen1cole58@gmail.com>
2025-02-13 06:27:51 +0300

    Added Act1

---
+++ Lab_act1_Arithmetics.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Prelim;
+
+/**
+ *
+ * @author oscar
+ */
+public class Lab_act1_Arithmetics {
+    
+    public static void main (String[]args){
+        
+        int x = 10;
+        int y = 4;
+        int z = 6;
+        
+        System.out.println("x" + "x" + "y" + "+" + "z" + "=" + (x*y+z));
+        System.out.println("(x" + "-" + "y)" + "%" + "z" + "=" + (x-y) %z);
+        System.out.println("(x" + "+" + "y)" + "+" + "z)" + "/" + "3" + "=" + (x+y+z) /3);
+        System.out.println("x" + "x" + "z" + "-" + "(y" + "x" + "y)" + "=" + (x * z - (y * y)));
+        
+        
+    }
+    
+}
