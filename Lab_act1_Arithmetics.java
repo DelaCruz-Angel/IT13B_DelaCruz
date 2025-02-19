000a08c59b18f99496efd63c92ffed80cfb4750c
DelaCruz-Angel <mobilen1cole58@gmail.com>
2025-02-19 10:10:22 +0800

    Update Lab_act1_Arithmetics.java

--- Lab_act1_Arithmetics.java
+++ Lab_act1_Arithmetics.java
@@ -1,29 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Prelim;
 
-/**
- *
- * @author oscar
- */
+import java.util.Scanner;
+
 public class Lab_act1_Arithmetics {
-    
-    public static void main (String[]args){
+    public static void main(String[] args){
         
-        int x = 10;
-        int y = 4;
-        int z = 6;
+        int first_number,second_number,third_number;
+        int x = 3;
+       
+        Scanner in = new Scanner(System.in);
         
-        System.out.println("x" + "x" + "y" + "+" + "z" + "=" + (x*y+z));
-        System.out.println("(x" + "-" + "y)" + "%" + "z" + "=" + (x-y) %z);
-        System.out.println("(x" + "+" + "y)" + "+" + "z)" + "/" + "3" + "=" + (x+y+z) /3);
-        System.out.println("x" + "x" + "z" + "-" + "(y" + "x" + "y)" + "=" + (x * z - (y * y)));
+        System.out.print("Enter first number: ");
+        first_number = in.nextInt();
+        System.out.print("Enter second number: ");
+        second_number = in.nextInt();
+        System.out.print("Enter third number: ");
+        third_number = in.nextInt();
         
-        
+        System.out.println(first_number + " * " + second_number + " + " + third_number + " = " + (first_number * second_number + third_number));
+        System.out.println("(" + first_number + " - " + second_number + ")" + " % " + third_number + " = " + ((first_number - second_number) % third_number));
+        System.out.println("(" + first_number + " + " + second_number + " + " + third_number + ")" + " / " + x + " = " + ((first_number + second_number + third_number)/ x));
+        System.out.println(first_number + " * " + third_number + " - " + "(" + second_number + " * " + second_number + ")" +  " = " + (first_number * third_number - (second_number * second_number)));
     }
-    
 }
